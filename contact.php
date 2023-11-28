0347a50fc39cc4d2037f9f589350b72acbf4549d
Javier Zam <javier_zam@yahoo.co.id>
2023-11-28 14:16:17 +0700

    update contact

--- contact.php
+++ contact.php
@@ -1,21 +1,25 @@
 <?php
-if($_POST) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+include_once('assets/vendor/php-email-form/php-email-form.php');
 
-    $toEmail = 'javier_zam@yahoo.co.id';
-    $emailSubject = 'Pesan Baru Dari: '.$name;
-    $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+if ($_POST) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $subject = $_POST['subject'];
+   $message = $_POST['message'];
 
-    $bodyParagraphs = ["Nama: {$name}", "Email: {$email}", "Pesan:", $message];
-    $body = join(PHP_EOL, $bodyParagraphs);
+   $toEmail = 'javier_zam@yahoo.co.id';
+   $emailSubject = 'Pesan Baru Dari: ' . $name;
+   $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
 
-    if(mail($toEmail, $emailSubject, $body, $headers)) {
-        echo 'Pesan mu telah terkirim, Terima Kasih!';
-    } else {
-        echo 'Ada masalah saat mengirim email. Silakan coba lagi nanti.';
-    }
+   $bodyParagraphs = ["Nama: {$name}", "Email: {$email}", "Pesan:", $message];
+   $body = join(PHP_EOL, $bodyParagraphs);
+
+   $emailForm = new Email_Form($toEmail, $email, $emailSubject, $body);
+
+   if ($emailForm->send()) {
+      echo 'Pesan mu telah terkirim, Terima Kasih!';
+   } else {
+      echo 'Ada masalah saat mengirim email. Silakan coba lagi nanti.';
+   }
 }
 ?>
